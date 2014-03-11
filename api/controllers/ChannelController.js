aed86a5fc95c01e211c165661f8ff75a68e6bc25
Daniel Vicory <dvicory@gmail.com>
2014-03-11 01:36:30 -0700

    Cleanup in Channel controller

--- api/controllers/ChannelController.js
+++ api/controllers/ChannelController.js
@@ -7,51 +7,49 @@
 
 var ChannelController = {
   index: function(req, res) {
-    
+
     Channel.find().populate('owner').exec(function(err, channels) {
-      if (err) { 
-        sails.log.error('Channel#index: ', err);
-        return res.serverError('DB error'); 
+      if (err) {
+        sails.log.error('Channel#index: DB error', err);
+        return res.serverError('DB error');
       }
 
-      if (req.wantsJSON || req.isSocket) { 
+      if (req.wantsJSON || req.isSocket) {
         return res.json({
           channels: channels
         });
-
       } else {
         return res.view({
           channels: channels,
           title: "Channels"
         });
       }
-    });  
+    });
   },
-  
+
   show: function(req, res) {
-    
+
     Channel.findOneById(req.param('id')).populate('owner').exec(function(err, channel) {
-      if (err) { 
-        sails.log.error('Channel#show: ', err);
-        return res.serverError('DB error'); 
+      if (err) {
+        sails.log.error('Channel#show: DB error', err);
+        return res.serverError('DB error');
       }
 
       if (!channel) {
         return res.notFound('Channel Not Found');
       }
-      
+
       var isBroadcaster = false;
-      if (req.wantsJSON || req.isSocket) { 
+      if (req.wantsJSON || req.isSocket) {
         return res.json({
           channel: channel
-        }); 
-
+        });
       } else {
         return res.view({
           channel: channel,
           title: channel.name
-        }); 
-      }   
+        });
+      }
     });
   }
 

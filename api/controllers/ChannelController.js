e413914530873674dca41cb251e23003733d8004
Jerry <jerryfoxyt@gmail.com>
2014-04-15 05:34:23 -0700

    Changes to channel controllers setting up the visual peer tree

--- api/controllers/ChannelController.js
+++ api/controllers/ChannelController.js
@@ -6,6 +6,43 @@
  */
 var Promise = require('bluebird');
 
+// Recusrive helper function 
+function buildtree(peer, treeJSON) {
+
+  //rootchildren = peer.forEach(getChildren)
+
+  // get an array of children (peer) connections
+  childrenConnections = peer.getChildren();
+
+  children = {};
+
+  // loop through the peer connections finding the peer object
+  // and adding it to the children array based on their endpoint id 
+  for(var j = 0; i < childrenConnections.length; j++) {
+    children.push(findChildreByPeerId(childrenConnections[i].endpoint.id));
+  }
+
+
+  for(var i = 0; i < children.length; i++) {
+    // check to see if returned empty object 
+    // empty object means no more children
+    if(!_.isEmpty(children[i])) {
+      
+      var temp = {"name": children[i].user.name,
+                  "children": []
+                  };
+
+      // add it to the children array            
+      treeJSON.children.push(temp);
+
+      nextTreelevel = treeJSON.children[i];
+
+      buildtree(peer, nextTreelevel);
+    }
+  }
+}
+
+
 var ChannelController = {
   index: function(req, res) {
 
@@ -72,12 +109,41 @@ var ChannelController = {
 
     }).error(function(e) {
       sails.log.error('Auth#create: DB error', e);
-      return res.json({ error: 'DB error' }, 500); 
+      return res.json({ error: 'DB error' }, 500);
     });
 
+  },
 
-  }
+  treeCreate: function(req, res) {
+    if (!req.isSocket) {
+      return res.badRequest('Peer management only supported with sockets');
+    }
 
-}
+    var socketId = req.socket.id;
+    var channelId = req.param('channel');
+
+    // find the peer who is the broadcaster & begin from there 
+    // TODO - might have to move this before helper function 
+    var root = Peer.findOne({channel: channelId , broadcaster: true})
+    .then( function(peer) {
+
+      // create init object with the root 
+      treeJSON = [
+        { "name": "broadcaster - " + peer.user.name,
+          "children" : []
+        }
+      ]
+
+      // now for the recursive magic 
+      buildtree(root, treeJSON[0]);
+
+      // ready to be served
+      return res.json(treeJSON);
+
+    });
+
+  },
+
+};
 
 module.exports = ChannelController;

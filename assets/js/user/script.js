2d3115372990a3e06042c57879740007a41322a0
Omar Masri <marismaro@gmail.com>
2014-04-22 02:23:22 -0700

    Added channel management controls to user profile.

--- assets/js/user/script.js
+++ assets/js/user/script.js
@@ -2,3 +2,15 @@
 if($('#flash').text() == "updated successfully") {
   $('#flash').attr('style','color: green;');
 }
+
+
+// Channel management dialog box
+$('.modifyChannel').click(function(e) {
+	$('#myModal').modal('show');
+	//console.log($(this).data('channel-name'));
+	var name = $(this).data('channel-name');
+	var description = $(this).data('channel-description');
+	$('#channelName').val(name);
+	$('#channelDescription').val(description);
+
+});

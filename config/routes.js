9530fdf5bb885df7c39768a94cfede6163fa1980
Jerry Medina <jerryfoxyt@gmail.com>
2014-04-22 05:29:22 -0700

    Latest channel tree visualisation, can not pull actual data without infinite looping for now

--- config/routes.js
+++ config/routes.js
@@ -46,6 +46,11 @@ module.exports.routes = {
     action: 'show'
   },
 
+  'get /channel/:id/tree': {
+    controller: 'Channel',
+    action: 'tree'
+  }
+
   // If a request to a URL doesn't match any of the custom routes above, it is matched
   // against Sails route blueprints.  See `config/blueprints.js` for configuration options
   // and examples.
